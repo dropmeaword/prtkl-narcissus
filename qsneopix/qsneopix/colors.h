541b5662ff4e221abf8f8fcf94731e6965825037
Luis Fernandez <dropmeaword@gmail.com>
2015-11-29 17:05:10 +0100

    support for multiple strips with different chipsets

--- qsneopix/qsneopix/colors.h
+++ qsneopix/qsneopix/colors.h
@@ -1,61 +1,66 @@
 
-void pattern_bounce() {
+void pattern_bounce(CRGB *mleds) {
   int lum = 255;
   for(int i=0;i<NUMBERPIXELS;i++){
     //strip.setPixelColor(i, lum, lum, lum);
     //strip.show();
-    leds[i].r = lum;
-    leds[i].g = lum;
-    leds[i].b = lum;
+    mleds[i].r = lum;
+    mleds[i].g = lum;
+    mleds[i].b = lum;
     FastLED.show();
     delay(4);
   }
 
-/*  for(int i=0;i<NUMBERPIXELS;i++){
-    strip.setPixelColor(i, 0, 0, 0);
-  }*/
+//  for(int i=0;i<NUMBERPIXELS;i++){
+//    mleds[i].r = 0;
+//    mleds[i].g = 0;
+//    mleds[i].b = 0;
+//    FastLED.show();
+//    delay(4);
+//  }
+  
   FastLED.show();
   //strip.show();
 }
 
-void pattern_bright(int lum) {
+void pattern_bright(CRGB *mleds, int lum) {
   for(int i=0;i<NUMBERPIXELS;i++){
     //strip.setPixelColor(i, lum, lum, lum);
-    leds[i].r = lum;
-    leds[i].g = lum;
-    leds[i].b = lum;
+    mleds[i].r = lum;
+    mleds[i].g = lum;
+    mleds[i].b = lum;
   }
   FastLED.show();
   //strip.show();
 }
 
-void pattern_red(int lum){
+void pattern_red(CRGB *mleds, int lum){
   for(int i=0;i<NUMBERPIXELS;i++){
-    leds[i].r = lum;
-    leds[i].g = 0;
-    leds[i].b = 0;
+    mleds[i].r = lum;
+    mleds[i].g = 0;
+    mleds[i].b = 0;
     //strip.setPixelColor(i, lum, 0, 0); 
   }
   //strip.show();
   FastLED.show();
 }
 
-void pattern_green(int lum){
+void pattern_green(CRGB *mleds, int lum){
   for(int i=0;i<NUMBERPIXELS;i++){
-    leds[i].r = 0;
-    leds[i].g = lum;
-    leds[i].b = 0;
+    mleds[i].r = 0;
+    mleds[i].g = lum;
+    mleds[i].b = 0;
     //strip.setPixelColor(i,0,lum,0);
   }
   //strip.show();
   FastLED.show();
 }
 
-void pattern_blue(int lum){
+void pattern_blue(CRGB *mleds, int lum){
   for(int i=0;i<NUMBERPIXELS;i++){
-    leds[i].r = 0;
-    leds[i].g = 0;
-    leds[i].b = lum;
+    mleds[i].r = 0;
+    mleds[i].g = 0;
+    mleds[i].b = lum;
     //strip.setPixelColor(i,0,0,lum);
   }
   //strip.show();
@@ -63,17 +68,17 @@ void pattern_blue(int lum){
 }
 
 
-void pattern_test() {
+void pattern_test(CRGB *mleds) {
   int lum = 10;
 
   delay(100);
-  pattern_red(lum);
+  pattern_red(mleds, lum);
   delay(1000);
-  pattern_green(lum);
+  pattern_green(mleds, lum);
   delay(1000);
-  pattern_blue(lum);
+  pattern_blue(mleds, lum);
   delay(500);
-  pattern_bounce();
+  pattern_bounce(mleds);
 
 /*
   for(int i = 0; i < NUMBERIDS; i++) {
